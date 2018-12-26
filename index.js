036047ec8c689604e2246a56e7f0793e33e7f745
Maximillian Arruda <marruda@opentext.com>
2018-12-26 16:57:09 -0200

    Added a new NumberOfChildrenByParentId2 function that doesn't iterate over into all items N times for calculates the children count

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
 /**
  * Returns the number of direct and indirect children from a given array of objects regarding a given parent id
- * 
- * @param {Array<{id:number;parendId:number}>} items 
- * @param {number} parentId 
+ *
+ * @param {Array<{id:number;parendId:number}>} items
+ * @param {number} parentId
  * @returns {number}
  */
 export function NumberOfChildrenByParentId(items, parentId) {
+  // checking if items is valid array
+  if (!Array.isArray(items)) return 0
 
-    // checking if items is valid array 
-    if (!Array.isArray(items))
-        return 0;
+  // getting the direct children
+  const directChildren = items.filter(item => item['parentId'] === parentId)
 
-    // getting the direct children  
-    const directChildren = items.filter(item => item['parentId'] === parentId);
+  if (!directChildren.length) {
+    // returns zero if there is no direct children. Right here any recursive invocation is stopped.
+    return 0
+  }
 
-    if (!directChildren.length) {
-        // returns zero if there is no direct children. Right here any recursive invocation is stopped.
-        return 0;
+  return (
+    directChildren.length +
+    directChildren
+      // getting the number of indirect children for each direct children. Right here starts a recursive invocation
+      .map(currentItem => NumberOfChildrenByParentId(items, currentItem['id']))
+      // accounting the numbers of indirect children discovered with the direct children number
+      .reduce((acc, val) => acc + val)
+  )
+}
+
+
+
+/**
+ * Returns the number of direct and indirect children from a given array of objects regarding a given parent id
+ *
+ * @param {Array<{id:number;parendId:number}>} items
+ * @param {number} parentId
+ * @returns {number}
+ */
+export function NumberOfChildrenByParentId2(items, parentId) {
+  // checking if items is valid array
+  if (!Array.isArray(items)) return 0
+
+  // object instance that represents a mapping of all items and its children
+  const map = {}
+  // flag to mark if it shoulds compute
+  let shouldCompute = false
+
+  // mapping all items and its children
+  items.forEach(item => {
+    if (item['id'] != null) { // avoiding objects without a valid 'id' attribute
+      if (!map[item['id']]) { // checking if the current id is mapped already 
+         map[item['id']] = [] // mapping the current id to an empty array of children
+      }
     }
 
-    return directChildren.length +
-        directChildren
-        // getting the number of indirect children for each direct children. Right here starts a recursive invocation
-        .map(currentItem => NumberOfChildrenByParentId(items, currentItem['id']))
-        // accounting the numbers of indirect children discovered with the direct children number
-        .reduce((acc, val) => acc + val);
+    if (item['id'] != item['parentId']) { // ignoring possible cyclic references
+      if (item['parentId'] != null) { // ignoring objects with invalid 'parentId' attribute
+        if (!shouldCompute) {   // once it's checked, do nothing 
+          shouldCompute = parentId === item['parentId']  // checking if it's should compute
+        }
+        const key = `${item['parentId']}` // creating a const var in order to keep the current parent id 
+        if (map[key]) { // checking if there is an array already mapped for the current parent id
+          map[key].push(item) // adding the current item to the parent id array
+        } else {
+          map[key] = [item] // mapping the current item to a new array to a given parent id that wasn't mapped yet
+        }
+      }
+    }
+  })
 
+  // internal function used just for count the mapped items with their children for a given item array
+  function recursiveCount(map, items) {
+    let count = 0
+    items.forEach(item => {
+      if (map[item['id']]) {
+        count += map[item['id']].length + recursiveCount(map, map[item['id']])
+      }
+    })
+    return count
+  }
+
+  let count = 0  // variable that stores the whole children count
+
+  if (shouldCompute) { // checking it it's needed to compute
+    // accumulating the direct children count with its children count
+    count += map[parentId].length + recursiveCount(map, map[parentId])
+  }
+
+  return count
 }
+
--- index.test.js
+++ index.test.js
@@ -1,71 +1,73 @@
-import {
-    NumberOfChildrenByParentId
-} from "./index"
+import * as funcs from './index'
 
-describe(`Testing ${NumberOfChildrenByParentId.name} function`, () => {
+for (const fn in funcs) {
+  const func = funcs[fn]
+  if (typeof func === 'function') {
+    describe(`Testing ${func.name} function`, () => {
+      const objectArray = [
+        { id: 0, parentId: null },
+        { id: 4, parentId: 3 },
+        { id: 1, parentId: 0 },
+        { id: 3, parentId: 2 },
+        { id: 2, parentId: 0 }
+      ]
 
-    const objectArray = [
-        { id: 0, parentId: null},
-        { id: 4, parentId: 3},
-        { id: 1, parentId: 0},
-        { id: 3, parentId: 2},
-        { id: 2, parentId: 0}
-    ];
-
-    const scenarios = [
+      const scenarios = [
         {
-            "items": objectArray,
-            "givenId": 3,
-            "expectedValue": 1
+          items: objectArray,
+          givenId: 0,
+          expectedValue: 4
         },
         {
-
-            "items": objectArray,
-            "givenId": 0,
-            "expectedValue": 4
+          items: objectArray,
+          givenId: 1,
+          expectedValue: 0
         },
         {
-
-            "items": objectArray,
-            "givenId": 2,
-            "expectedValue": 2
+          items: objectArray,
+          givenId: 2,
+          expectedValue: 2
         },
         {
-
-            "items": objectArray,
-            "givenId": 1,
-            "expectedValue": 0
+          items: objectArray,
+          givenId: 3,
+          expectedValue: 1
         },
         {
-
-            "items": objectArray,
-            "givenId": 4,
-            "expectedValue": 0
+          items: objectArray,
+          givenId: 4,
+          expectedValue: 0
         },
         {
-
-            "items": [],
-            "givenId": 1,
-            "expectedValue": 0
+          items: [],
+          givenId: 1,
+          expectedValue: 0
         },
         {
-
-            "items": null,
-            "givenId": 2,
-            "expectedValue": 0
+          items: null,
+          givenId: 1,
+          expectedValue: 0
         },
         {
-
-            "items": null,
-            "givenId": null,
-            "expectedValue": 0
+          items: null,
+          givenId: null,
+          expectedValue: 0
         }
-    ];
+      ]
 
-    scenarios.forEach(scenario => {
-        it(`Given the array:\n\t${JSON.stringify(scenario.items)}\n\tCase it's passed '${scenario.givenId}' as parent id then the number of children should be equals to ${scenario.expectedValue}`, () => {
-            expect(NumberOfChildrenByParentId(scenario.items, scenario.givenId)).toBe(scenario.expectedValue);
-        });
-    });
-
-});
+      scenarios.forEach(scenario => {
+        it(`Given the array:\n\t${JSON.stringify(
+          scenario.items
+        )}\n\tCase it's passed '${
+          scenario.givenId
+        }' as parent id then the number of children should be equals to ${
+          scenario.expectedValue
+        }`, () => {
+          expect(func(scenario.items, scenario.givenId)).toBe(
+            scenario.expectedValue
+          )
+        })
+      })
+    })
+  }
+}
